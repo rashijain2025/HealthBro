069f630ca01c252579859407337821ccbf82c10c
Rashi Jain <jrashi813@gmail.com>
2025-07-17 21:23:17 +0530

    Frontend of HealthBro

---
+++ HealthBro/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Login from './Login'
+import Registration from './Registration'
+
+
+
+function App() {
+  
+  return (
+    <>
+    <Login />
+    {/* <Registration /> */}
+
+      
+    </>
+  )
+}
+
+export default App
